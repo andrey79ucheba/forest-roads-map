1ab62f39345fccb4f2bca670b8393c8e23d810b3
AndreyCh79 <andrey.chuvashev@bk.ru>
2026-09-08 23:42:51 +0300

    Fix: Use internal DB host and disable SSL

--- backend/forest_roads/settings_prod.py
+++ backend/forest_roads/settings_prod.py
@@ -71,7 +71,7 @@ DATABASES = {
         'HOST': os.environ.get('DB_HOST', 'localhost'),
         'PORT': os.environ.get('DB_PORT', '5432'),
         'OPTIONS': {
-            'sslmode': 'require',
+            'sslmode': 'disable',
         },
     }
 }
